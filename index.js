46c3a03faf0d2baacfbc03b50eb8f8b0560ae0bb
andtrno1 <andtrno1@gmail.ccom>
2022-04-28 01:09:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoader", function(){
+  textUpdate()  
+})
+
+function textUpdate(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
